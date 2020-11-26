9a0339246d42f309f3c9a2b1f028cbcdf3fbf0fd
2733284198 <2733284198@qq.com>
2020-11-25 21:36:46 -0800

    2020-11-25 commit 1 redis 2 snowflake 3 db 4 model:get(), find[1,2,3], delete() 5 delete 6 model: getbyid, getall 7 session 8 validatedctl.php 9 FooRequest.php 10 upload:oss file

--- app/Controller/validatedctl.php
+++ app/Controller/validatedctl.php
@@ -8,7 +8,9 @@ use App\Request\FooRequest;
 use Hyperf\HttpServer\Annotation\AutoController;
 use Hyperf\HttpServer\Contract\RequestInterface;
 use Hyperf\HttpServer\Contract\ResponseInterface;
-use mysql_xdevapi\Exception;
+
+use Hyperf\Di\Annotation\Inject;
+use Hyperf\Validation\Contract\ValidatorFactoryInterface;
 
 /**
  * @AutoController
@@ -18,9 +20,70 @@ use mysql_xdevapi\Exception;
 
 class validatedctl
 {
+    /**
+     * @Inject()
+     * @var ValidatorFactoryInterface
+     */
+    protected $validationFactory;
+
     public function index2(RequestInterface $request, ResponseInterface $response)
     {
         return $response->raw('Hello Hyperf!');
+    }
+
+    public function check(RequestInterface $request)
+    {
+        $validator = $this->validationFactory->make(
+            $request->all(),
+            [
+                'foo' => 'required',
+                'bar' => 'required',
+            ],
+            [
+                'foo.required' => 'foo is required',
+                'bar.required' => 'bar is required',
+            ]
+        );
+
+        if ($validator->fails()){
+            // Handle exception
+            $errorMessage = $validator->errors()->first();
+
+            return $errorMessage;
+        }
+
+        return $request->all();
+    }
+
+    public function foo(RequestInterface $request)
+    {
+        $validator = $this->validationFactory->make(
+            $request->all(),
+            [
+                'foo' => 'required',
+                'bar' => 'required',
+            ],
+            [
+                'foo.required' => 'foo is required',
+                'bar.required' => 'bar is required',
+            ]
+        );
+
+        $validator->after(function ($validator) {
+            if ($this->somethingElseIsInvalid()) {
+                $validator->errors()->add('field', 'Something is wrong with this field!');
+            }
+        });
+
+        if ($validator->fails()) {
+            //
+            echo '<hr>fails';
+        }
+    }
+
+    public function somethingElseIsInvalid()
+    {
+        echo '<hr>somethingElseIsInvalid';
     }
 
     public function index(FooRequest $request)
